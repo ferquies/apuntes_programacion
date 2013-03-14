7e3a87a749a527512510866b3f6905631a03fb14
Fernando Quilez Esquitino <ferquies@gmail.com>
2013-03-14 12:37:29 +0100

    añadido TestFichero.java, escribe un String en un fichero de texto

---
+++ Ej T7/TestFichero.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class TestFichero {
+	public static void main(String[] args) {
+		FileOutputStream f = null;
+		String s = "En un lugar de la mancha de cuyo nombre no quiero acordarme...";
+		char c = 0;
+		try {
+			f = new FileOutputStream("datos.txt");
+			for(int i = 0; i < s.length(); i++) {
+				c = s.charAt(i);
+				f.write((byte)c);
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				f.close();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
